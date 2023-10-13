d7afd045385d8fc5190dd55cc1eed6cec93e91c1
xaaane <xaneslime@gmail.com>
2023-10-13 14:02:46 -0400

    good enough for now

--- src/routes/media/media.page.jsx
+++ src/routes/media/media.page.jsx
@@ -5,89 +5,90 @@ import mediaImageMap from "../../../mediaImageMap.json";
 import "./media.styles.scss";
 
 const Media = () => {
-	// image path
-	const mediaImagePath = "src/routes/media/components/single/images/";
+  // image path
+  const mediaImagePath = "src/routes/media/components/single/images/";
 
-	//handle lightbox
-	const [images, setImages] = useState([]);
-	const [folder, setFolder] = useState("");
-	const [showLightbox, setShowLightbox] = useState(false);
-	const lightbox = useRef(null);
-	const handleOpen = (media) => {
-		setFolder(media.folder);
-		const newImages = mediaImageMap[media.folder];
-		setImages(newImages);
-		setShowLightbox(true);
-	};
-	const handleClose = () => {
-		setImages([]);
-		setCurrentIndex(0);
-		setShowLightbox(false);
-	};
-	useEffect(() => {
-		if (showLightbox) {
-			const scrollToLightbox = lightbox.current;
-			scrollToLightbox.scrollIntoView({
-				behavior: "smooth",
-			});
-		}
-	}, [showLightbox]);
-	// Handle image indexing and scrolling
-	const [currentIndex, setCurrentIndex] = useState(0);
-	// const handleNext = () => {
-	// 	setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-	// };
-	// const handlePrev = () => {
-	// 	setCurrentIndex(
-	// 		(prevIndex) => (prevIndex - 1 + images.length) % images.length
-	// 	);
-	// };
-	const handleImageClick = (index) => {
-		setCurrentIndex(index);
-	};
+  //handle lightbox
+  const [images, setImages] = useState([]);
+  const [folder, setFolder] = useState("");
+  const [showLightbox, setShowLightbox] = useState(false);
+  const lightbox = useRef(null);
+  const handleOpen = (media) => {
+    setFolder(media.folder);
+    const newImages = mediaImageMap[media.folder];
+    setImages(newImages);
+    setShowLightbox(true);
+  };
+  const handleClose = () => {
+    setImages([]);
+    setCurrentIndex(0);
+    setShowLightbox(false);
+  };
+  useEffect(() => {
+    if (showLightbox) {
+      const scrollToLightbox = lightbox.current;
+      scrollToLightbox.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  }, [showLightbox]);
 
-	const imageRow = useRef(null);
+  // Handle image indexing and scrolling
+  const [currentIndex, setCurrentIndex] = useState(0);
+  // const handleNext = () => {
+  // 	setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  // };
+  // const handlePrev = () => {
+  // 	setCurrentIndex(
+  // 		(prevIndex) => (prevIndex - 1 + images.length) % images.length
+  // 	);
+  // };
+  const handleImageClick = (index) => {
+    setCurrentIndex(index);
+  };
 
-	const handleScrollRight = () => {
-		const container = imageRow.current;
-		container.scrollBy({ left: 200, behavior: "smooth" });
-	};
+  const imageRow = useRef(null);
 
-	const handleScrollLeft = () => {
-		const container = imageRow.current;
-		container.scrollBy({ left: -200, behavior: "smooth" });
-	};
+  const handleScrollRight = () => {
+    const container = imageRow.current;
+    container.scrollBy({ left: 200, behavior: "smooth" });
+  };
 
-	return (
-		<>
-			<section id="media-section">
-				<div
-					id="media-container"
-					className="container"
-					style={{ display: showLightbox ? "none" : "grid" }}
-				>
-					{mediaData.map((media) => (
-						<Single
-							key={media.title}
-							image={`${mediaImagePath}${media.folder}/${
-								mediaImageMap[media.folder][0]
-							}`}
-							title={media.title}
-							date={media.date}
-							onClick={() => handleOpen(media)}
-						/>
-					))}
-				</div>
+  const handleScrollLeft = () => {
+    const container = imageRow.current;
+    container.scrollBy({ left: -200, behavior: "smooth" });
+  };
 
-				<section
-					id="lightbox"
-					style={{ display: showLightbox ? "flex" : "none" }}
-					ref={lightbox}
-				>
-					<div id="lightbox-container">
-						<div id="images-container">
-							<div id="image">
-								{/* <div id="nav-arrows">
+  return (
+    <>
+      <section id="media-section">
+        <div
+          id="media-container"
+          className="container"
+          style={{ display: showLightbox ? "none" : "grid" }}
+        >
+          {mediaData.map((media) => (
+            <Single
+              key={media.title}
+              image={`${mediaImagePath}${media.folder}/${
+                mediaImageMap[media.folder][0]
+              }`}
+              title={media.title}
+              date={media.date}
+              onClick={() => handleOpen(media)}
+            />
+          ))}
+        </div>
+
+        <section
+          id="lightbox"
+          style={{ display: showLightbox ? "flex" : "none" }}
+          ref={lightbox}
+        >
+          <div id="lightbox-container">
+            <div id="images-container">
+              <div id="image">
+                {/* <div id="nav-arrows">
 									<i
 										className="fa-regular fa-arrow-left"
 										role="button"
@@ -99,49 +100,56 @@ const Media = () => {
 										onClick={handleNext}
 									></i>
 								</div> */}
-								<div id="single-image-container">
-									<img
-										src={`${mediaImagePath}${folder}/${images[currentIndex]}`}
-										alt={images[currentIndex]}
-									/>
-									<button onClick={handleClose}>
-										<i className="fa-sharp fa-solid fa-xmark"></i>
-									</button>
-								</div>
-							</div>
-							<div id="images">
-								<div id="gradient"></div>
-								<div id="left-arrow">
-									<i
-										className="fa-regular fa-arrow-left"
-										role="button"
-										onClick={handleScrollLeft}
-									></i>
-								</div>
-								<div id="right-arrow">
-									<i
-										className="fa-regular fa-arrow-right"
-										role="button"
-										onClick={handleScrollRight}
-									></i>
-								</div>
-								<div id="image-row-container" ref={imageRow}>
-									{images.map((img, index) => (
-										<img
-											key={index}
-											src={`${mediaImagePath}${folder}/${img}`}
-											alt={img}
-											onClick={() => handleImageClick(index)}
-										/>
-									))}
-								</div>
-							</div>
-						</div>
-					</div>
-				</section>
-			</section>
-		</>
-	);
+                <div id="single-image-container">
+                  {/* <div id="test">
+                    <img
+                      src={`${mediaImagePath}${folder}/${images[currentIndex]}`}
+                      alt={images[currentIndex]}
+                    />
+                  </div> */}
+                  <img
+                    src={`${mediaImagePath}${folder}/${images[currentIndex]}`}
+                    alt={images[currentIndex]}
+                  />
+
+                  <button onClick={handleClose}>
+                    <i className="fa-sharp fa-solid fa-xmark"></i>
+                  </button>
+                </div>
+              </div>
+              <div id="images">
+                <div id="gradient"></div>
+                <div id="left-arrow">
+                  <i
+                    className="fa-regular fa-arrow-left"
+                    role="button"
+                    onClick={handleScrollLeft}
+                  ></i>
+                </div>
+                <div id="right-arrow">
+                  <i
+                    className="fa-regular fa-arrow-right"
+                    role="button"
+                    onClick={handleScrollRight}
+                  ></i>
+                </div>
+                <div id="image-row-container" ref={imageRow}>
+                  {images.map((img, index) => (
+                    <img
+                      key={index}
+                      src={`${mediaImagePath}${folder}/${img}`}
+                      alt={img}
+                      onClick={() => handleImageClick(index)}
+                    />
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </section>
+    </>
+  );
 };
 
 export default Media;
