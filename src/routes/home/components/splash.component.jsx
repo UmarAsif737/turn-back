be39c907b73b704afcc7bfdd9d76f28d8245fb1f
red-slime <xaneslime@gmail.com>
2023-10-16 15:32:15 -0400

    replaced direct dom manip with useRef in Splash, maybe that will fix this weird bug

--- src/routes/home/components/splash.component.jsx
+++ src/routes/home/components/splash.component.jsx
@@ -1,17 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import "./splash.styles.scss";
 
 const Splash = () => {
+	// Woah now, can't manipulate the DOM directly!
+	const firstCopyLeftRef = useRef(null);
+	const secondCopyLeftRef = useRef(null);
+	const firstCopyRightRef = useRef(null);
+	const secondCopyRightRef = useRef(null);
+	const firstCopyMobileRef = useRef(null);
+	const secondCopyMobileRef = useRef(null);
 	// Scrolling image backgrounds
 	useEffect(() => {
-		const firstCopyLeft = document.getElementById("first-copy-left");
-		const secondCopyLeft = document.getElementById("second-copy-left");
-		const firstCopyRight = document.getElementById("first-copy-right");
-		const secondCopyRight = document.getElementById("second-copy-right");
-		const firstCopyMobile = document.getElementById("first-copy-mobile");
-		const secondCopyMobile = document.getElementById("second-copy-mobile");
-
 		let scrollSpeed = 0.2;
 		let firstPos = 0;
 		let secondPos = -100;
@@ -28,12 +28,12 @@ const Splash = () => {
 				secondPos = firstPos - 100;
 			}
 
-			firstCopyLeft.style.transform = `translateY(${firstPos}%)`;
-			secondCopyLeft.style.transform = `translateY(${secondPos}%)`;
-			firstCopyRight.style.transform = `translateY(${firstPos}%)`;
-			secondCopyRight.style.transform = `translateY(${secondPos}%)`;
-			firstCopyMobile.style.transform = `translateX(${firstPos}%)`;
-			secondCopyMobile.style.transform = `translateX(${secondPos}%)`;
+			firstCopyLeftRef.current.style.transform = `translateY(${firstPos}%)`;
+			secondCopyLeftRef.current.style.transform = `translateY(${secondPos}%)`;
+			firstCopyRightRef.current.style.transform = `translateY(${firstPos}%)`;
+			secondCopyRightRef.current.style.transform = `translateY(${secondPos}%)`;
+			firstCopyMobileRef.current.style.transform = `translateX(${firstPos}%)`;
+			secondCopyMobileRef.current.style.transform = `translateX(${secondPos}%)`;
 
 			requestAnimationFrame(animateScroll);
 		};
@@ -50,12 +50,12 @@ const Splash = () => {
 		<>
 			<main id="splash-container" className="get-scrolled-kid">
 				<div id="splash-background-mobile">
-					<div id="first-copy-mobile">
+					<div id="first-copy-mobile" ref={firstCopyMobileRef}>
 						<div id="image-one-mobile" className="bg-image center"></div>
 						<div id="image-two-mobile" className="bg-image center"></div>
 						<div id="image-three-mobile" className="bg-image center"></div>
 					</div>
-					<div id="second-copy-mobile">
+					<div id="second-copy-mobile" ref={secondCopyMobileRef}>
 						<div id="image-four-mobile" className="bg-image center"></div>
 						<div id="image-five-mobile" className="bg-image center"></div>
 						<div id="image-six-mobile" className="bg-image center"></div>
@@ -84,25 +84,41 @@ const Splash = () => {
 				</div>
 				<div id="splash-background-desktop">
 					<div id="left-container">
-						<div id="first-copy-left" className="first-copy">
+						<div
+							id="first-copy-left"
+							className="first-copy"
+							ref={firstCopyLeftRef}
+						>
 							<div id="left-image-one" className="bg-image left"></div>
 							<div id="left-image-two" className="bg-image left"></div>
 						</div>
-						<div id="second-copy-left" className="second-copy">
+						<div
+							id="second-copy-left"
+							className="second-copy"
+							ref={secondCopyLeftRef}
+						>
 							<div id="left-image-three" className="bg-image left"></div>
 							<div id="left-image-four" className="bg-image left"></div>
 						</div>
 					</div>
 					<div id="center-container"></div>
 					<div id="right-container">
-						<div id="first-copy-right" className="first-copy">
+						<div
+							id="first-copy-right"
+							className="first-copy"
+							ref={firstCopyRightRef}
+						>
 							<div id="right-image-one" className="bg-image right"></div>
 							<div id="right-image-two" className="bg-image right"></div>
 							<div id="right-image-three" className="bg-image right"></div>
 							<div id="right-image-four" className="bg-image right"></div>
 							<div id="right-image-five" className="bg-image right"></div>
 						</div>
-						<div id="second-copy-right" className="second-copy">
+						<div
+							id="second-copy-right"
+							className="second-copy"
+							ref={secondCopyRightRef}
+						>
 							<div id="right-image-six" className="bg-image right"></div>
 							<div id="right-image-seven" className="bg-image right"></div>
 							<div id="right-image-eight" className="bg-image right"></div>
