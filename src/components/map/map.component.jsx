47e4712568d1d04837cf015c09db5bcc8a017c8c
red-slime <xaneslime@gmail.com>
2023-10-23 10:32:16 -0400

    debugging file paths

--- src/components/map/map.component.jsx
+++ src/components/map/map.component.jsx
@@ -267,7 +267,8 @@ const Map = () => {
 													<a
 														className="plat"
 														// href={staticPlatPath + location.address + ".pdf"}
-														href={`${staticPlatPath}${location.address.toLowerCase()}.pdf`}
+														//href={`${staticPlatPath}${location.address.toLowerCase()}.pdf`}
+														href="/map-location-plats/218 Perkins St.pdf"
 														// target="_blank"
 													>
 														View Plat
