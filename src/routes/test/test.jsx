a0443375d5a4402b9214de03fa2a880c5774ebc7
red-slime <xaneslime@gmail.com>
2023-10-30 10:01:29 -0400

    testing forms

--- src/routes/test/test.jsx
+++ src/routes/test/test.jsx
@@ -1,19 +1,88 @@
 const TestForm = () => {
 	return (
 		<>
-			<form className="open" name="pre-application" method="post">
-				<input type="hidden" name="form-name" value="test-form" />
+			<form name="volunteer" method="post">
+				<input type="hidden" name="volunteer-netlify-input" value="volunteer" />
 				<label>
-					Primary Applicant Name<span className="required">*</span>
-					<input type="text" name="email" required />
+					Applicant Full Name<span className="required">*</span>
+					<input type="text" name="primary-name" required />
 				</label>
+				<label>
+					Guardian Full Name
+					<span className="thin"></span>
+					<input type="text" name="secondary-name" />
+				</label>
+				<label>
+					Street Address<span className="required">*</span>
+					<input type="text" name="address" required />
+				</label>
+				<label>
+					City<span className="required">*</span>
+					<input type="text" name="city" required />
+				</label>
+				<label>
+					State<span className="required">*</span>
+					<input
+						type="text"
+						name="state"
+						defaultValue="GA"
+						minLength="2"
+						maxLength="2"
+						required
+					/>
+				</label>
+				<label>
+					Zipcode<span className="required">*</span>
+					<input
+						type="text"
+						name="zipcode"
+						pattern="[0-9]{5}"
+						minLength="5"
+						maxLength="5"
+						required
+					/>
+				</label>
+				<label>
+					Phone Number<span className="required">*</span>
+					<input type="tel" name="phone" required />
+				</label>
+				<label>
+					Email Address<span className="required">*</span>
+					<input type="email" name="email" required />
+				</label>
+				<div className="checkbox-container">
+					<input type="checkbox" id="previously" />
+					<label htmlFor="previously">
+						I have volunteered with Turn Back the Block
+					</label>
+				</div>
+				<div className="checkbox-container">
+					<input type="checkbox" id="understand" />
+					<label htmlFor="understand">
+						and agree to abide by it<span className="required">*</span>
+					</label>
+				</div>
 
+				<label>
+					How did you hear about the program?
+					<span className="required">*</span>
+					<select name="hear-about" required>
+						<option>Website</option>
+						<option>Social Media</option>
+						<option>Signage</option>
+						<option>Friend / Family</option>
+					</select>
+				</label>
+				<label id="referral">
+					Friend's or Family's name:
+					<input type="text" name="referrer" />
+				</label>
 				<input
 					className="button red"
 					type="submit"
-					value="Submit"
+					value="Sign Up"
 					role="button"
-				></input>
+				/>
 			</form>
 		</>
 	);
